2e11829eb5241e34028c57ce84543a25a7ac6f3c
Raphael Javaux <raphaeljavaux@gmail.com>
2012-02-19 00:35:43 +0100

    quick check images

---
+++ tests/Tests/Vision/Image.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module Tests.Vision.Image (
+      tests
+    ) where
+
+import Control.Applicative
+import Test.QuickCheck
+
+import Vision.Primitive (Size (..))
+import qualified Vision.Image.RGBAImage as R
+import qualified Vision.Image.GreyImage as G
+
+import Tests.Config (maxImageSize)
+import qualified Tests.Vision.Primitive
+
+-- instance Arbitrary Image where
+--     arbitrary =
+--         Size <$> choose (0, maxImageSize) <*> choose (0, maxImageSize)
+        
+instance Arbitrary G.GreyImage where
+    arbitrary = do
+        [size@(Size w h)] <- vector 1 :: Gen [Size]
+        G.create size <$> vector (w * h)
+        
+-- instance Arbitrary IntegralImage where
+--     arbitrary =
+--         Size <$> choose (0, maxImageSize) <*> choose (0, maxImageSize)
+
+tests = {-conjoin [-}
+              label "Grey => RGBA => Grey" $ propGreyRGBA
+--             , label "Grey => Resize 200% => RGBA => Resize 50% => Grey" $
+--                 propGreyResize
+--         ]
+
+propGreyRGBA :: G.GreyImage -> Bool
+propGreyRGBA img = img == G.fromRGBA (G.toRGBA img)
+
+-- propGreyResize :: G.GreyImage -> Bool
+-- propGreyResize img =
+--     let Size w h = G.getSize img
+--         img' = G.fromRGBA $ flip R.resize (Size (w `quot` 2) (h `quot` 2))
+--              $ G.toRGBA   $ flip G.resize (Size (w * 2)      (h * 2)) $ img
+--     in img == img'
---
+++ tests/Tests/Vision/Primitive.hs
@@ -0,0 +1,20 @@
+module Tests.Vision.Primitive (
+      tests
+    ) where
+
+import Control.Applicative
+import Test.QuickCheck
+
+import Vision.Primitive (Size (..), sizeRange)
+
+import Tests.Config (maxImageSize)
+
+instance Arbitrary Size where
+    arbitrary =
+        Size <$> choose (0, maxImageSize) <*> choose (0, maxImageSize)
+
+tests = label "sizeRange length" propSizeRangeLength
+
+propSizeRangeLength :: Size -> Bool
+propSizeRangeLength size@(Size w h) =
+    length (sizeRange size) == w * h
